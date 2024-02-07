859336c566f68626c5ef014b1f892b39a0c7351d
NoorAli117 <nali1.karachi@gmail.com>
2024-02-07 17:35:09 +0500

    Initial Commit

---
+++ iTeckTest/iTeckTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTeckTestApp.swift
+//  iTeckTest
+//
+//  Created by Noor on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iTeckTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
